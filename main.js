e6d6ac6846e13aac21059c9efc40f3f0f9dee8fd
Jonathan Chen <jonoson@gmail.com>
2019-02-02 19:38:01 -0500

    update main.js to use a separate dom

--- main.js
+++ main.js
@@ -3,14 +3,18 @@ const devices = require('puppeteer/DeviceDescriptors');
 const fse = require('fs-extra'); // v 5.0.0
 const path = require('path');
 const beautify = require('js-beautify').html;
+const colors = require('colors');
+const { JSDOM } = require("jsdom");;
 
 const IMG_SELECTOR = 'amp-img';
 
-let url = 'http://127.0.0.1:8080/';
+let url = 'http://127.0.0.1:8080';
 let escapedUrl = url.replace('https://', '').replace('\/', '|');
 let envVars = {
-  '%%VAR_URL%%': encodeURI(url),
+  '%%URL%%': encodeURI(url),
 };
+let styleByUrls = {}, allStyles = '';
+var sourceDom = null;
 
 const steps = [
   {
@@ -25,27 +29,41 @@ const steps = [
       log: 'Remove inline scripts',
       actionType: 'replace',
       selector: 'html',
-      regex: '<script[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>',
+      regex: '(<!--)?.*<script[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>.*(-->)?',
       replace: '',
     }, {
       log: 'Remove third-party elements',
       actionType: 'replace',
       selector: 'html',
-      regex: '<.*(src|href)=(?!"(%%VAR_URL%%|#)).*>',
+      regex: '(<!--)?.*<(script|link) .*(src|href)=(?!"(%%URL%%|#)).*>.*(?:-->)?',
       replace: '',
     }],
-    beautify: true,
+  },
+  {
+    name: 'Inline all CSS styles in <head>',
+    actions: [{
+      log: 'append styles',
+      actionType: 'appendStyle',
+      selector: 'head',
+      excludeDomains: [],
+      attributes: ['amp-custom'],
+    }],
   },
   {
     name: 'Remove disallowed attributes',
     actions: [{
-      log: 'Remove onclick',
+      log: 'Remove attributes: onclick|controlheight|controlwidth|aria-description|adhocenable|data',
       actionType: 'replace',
       selector: 'html',
       regex: '(onclick|controlheight|controlwidth|aria-description|adhocenable|data-[^=]*)=\"[^"]*\"',
       replace: '',
+    }, {
+      log: 'Remove to|expect|of|mind|promise|menu:click|onclick|privacy-policy|policy|sign-in|nav:clicked|my-account',
+      actionType: 'replace',
+      selector: 'html',
+      regex: '(to|expect|of|mind|promise|menu:click|onclick|privacy-policy|policy|sign-in|nav:clicked|my-account)=\"[^"]*\"',
+      replace: '',
     }],
-    beautify: true,
   },
   {
     name: 'Make relative URLs absolute',
@@ -54,40 +72,73 @@ const steps = [
       actionType: 'replace',
       selector: 'html',
       regex: '(href|src)=\"([/a-zA-Z0-9\.]+)\"',
-      replace: '$1="%%VAR_URL%%$2"',
-    }],
-    beautify: true,
-  },
-  {
-    name: 'Set HTML tag with AMP',
-    actions: [{
+      replace: '$1="%%URL%%/$2"',
     }],
   },
   {
     name: 'Add AMP JS library and AMP boilerplate',
     actions: [{
+      log: 'Set HTML tag with AMP',
       actionType: 'setAttribute',
       selector: 'html',
       attribute: 'amp',
       value: '',
     }, {
+      log: 'Add AMP JS library.',
+      actionType: 'insertBottom',
+      selector: 'head',
+      value: '<script async src="https://cdn.ampproject.org/v0.js"></script>',
+    }, {
       log: 'Update viewport',
-      actionType: 'replaceOrInsert', // replaceOrInsert
+      actionType: 'replaceOrInsert',
       selector: 'head',
       regex: '<meta name="viewport"([\sa-zA-Z1-9=\"\-\,]*)>',
       replace: '<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">',
     }, {
-      log: 'Add AMP JS library.',
-      actionType: 'appendAfter',
-      selector: 'title',
-      value: '<script async src="https://cdn.ampproject.org/v0.js"></script>',
-    // }, {
-    //   log: 'Add AMP boilerplate.',
-    //   actionType: 'insertBottom',
-    //   selector: 'head',
-    //   value: '',
+      log: 'Update charset to UTF-8',
+      actionType: 'replaceOrInsert',
+      selector: 'head',
+      regex: '<meta charset=".*">',
+      replace: '<meta charset="utf-8">',
+    }, {
+      log: 'Add canonical link.',
+      actionType: 'insertBottom',
+      selector: 'head',
+      value: '<link rel=canonical href="%%URL%%">',
+    }, {
+      log: 'Add AMP boilerplate.',
+      actionType: 'insertBottom',
+      selector: 'head',
+      value: '<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>',
     }],
-    beautify: true,
+  },
+  {
+    name: 'Convert img to amp-img',
+    actions: [{
+      log: 'replace img to amp-img',
+      actionType: 'replace',
+      selector: 'html',
+      regex: '<img(.*)>(</img>)?',
+      replace: '<amp-img $1></amp-img>',
+    }, {
+      log: 'Set responsive layout',
+      actionType: 'setAttribute',
+      selector: 'amp-img',
+      attribute: 'width',
+      value: '1',
+    }, {
+      log: 'Set responsive layout',
+      actionType: 'setAttribute',
+      selector: 'amp-img',
+      attribute: 'height',
+      value: '1',
+    }, {
+      log: 'Set responsive layout',
+      actionType: 'setAttribute',
+      selector: 'amp-img',
+      attribute: 'layout',
+      value: 'responsive',
+    }],
   },
 ];
 
@@ -102,31 +153,58 @@ function replaceEnvVars(str) {
 
 async function outputToFile(filename, html, options) {
   let filePath = path.resolve(`./output/${escapedUrl}/${filename}`);
-  await fse.outputFile(filePath, html);
+  await fse.outputFile(filePath, '<!doctype html>\n' + html);
+}
+
+async function collectStyles(response) {
+  if(response.request().resourceType() === 'stylesheet') {
+    let url = await response.url();
+    let text = await response.text();
+    allStyles += text;
+    styleByUrls[url] = text;
+  }
 }
 
 async function run() {
+  let consoleOutput = '';
+
   const browser = await puppeteer.launch({
     headless: true
   });
   const page = await browser.newPage();
-  page.on('console', consoleObj => console.log(consoleObj.text()));
-
   await page.emulate(devices['Pixel 2']);
-  await page.goto(url);
-  await outputToFile(`output-step-0.html`, await page.content());
+  page.on('response', collectStyles);
+  page.on('console', (consoleObj) => {
+    // console.log(consoleObj.text())
+    consoleOutput += consoleObj.text() + '\n';
+  });
+
+  // Add #development to the URL.
+  if (url.match(/#.*=(\w)+/ig)) {
+    url = url.replace(/#.*=(\w)+/ig, '#development=1');
+  } else {
+    url += '#development=1';
+  }
+
+  // Open URL and save source to sourceDom.
+  const response = await page.goto(url);
+  let pageSource = await response.text();
+  sourceDom = new JSDOM(pageSource).window.document;
+  console.log(`Open ${url}`.green);
+  await outputToFile(`output-step-0.html`, pageSource);
   await page.screenshot({path: `output/${escapedUrl}/output-step-0.png`});
 
-  const self = {
-    outputToFile: outputToFile,
-  };
+  // Clear page.on listener.
+  page.removeListener('response', collectStyles);
 
   let i = 1;
   let stepOutput = '';
   for (let i=0; i<steps.length; i++) {
     let step = steps[i];
+    consoleOutput = '';
+
     if (!step.actions || step.skip) continue;
-    console.log(`=> Step ${i+1}: ${step.name}`);
+    console.log(`Step ${i+1}: ${step.name}`.yellow);
 
     step.actions.forEach(async (action) => {
       Object.keys(action).forEach((prop) => {
@@ -134,101 +212,110 @@ async function run() {
       });
 
       let message = action.actionType;
+      let el, html, regex, matches, newEl;
 
       switch (action.actionType) {
         case 'setAttribute':
-          message = await page.evaluate(async (action) => {
-            let el = document.querySelector(action.selector);
-            el.setAttribute(action.attribute, action.value);
-            return `set ${action.attribute} as ${action.value}`;
-          }, action);
+          el = sourceDom.querySelector(action.selector);
+          el.setAttribute(action.attribute, action.value);
+          message = `set ${action.attribute} as ${action.value}`;
           break;
 
         case 'replace':
-          message = await page.evaluate(async (action) => {
-            let el = document.querySelector(action.selector);
-            if (!el) return `No matched regex: ${action.selector}`;
+          el = sourceDom.querySelector(action.selector);
+          if (!el) return `No matched regex: ${action.selector}`;
 
-            let html = el.outerHTML;
-            let regex = new RegExp(action.regex, 'ig');
-            let matches = html.match(regex, 'ig');
-            html = html.replace(regex, action.replace);
-            el.innerHTML = html;
-            return `${matches ? matches.length : 0} replaced`;
-          }, action);
+          html = el.outerHTML;
+          regex = new RegExp(action.regex, 'ig');
+          matches = html.match(regex, 'ig');
+          html = html.replace(regex, action.replace);
+          el.innerHTML = html;
+          message = `${matches ? matches.length : 0} replaced`;
           break;
 
         case 'replaceOrInsert':
-          message = await page.evaluate(async (action) => {
-            let el = document.querySelector(action.selector);
-            if (!el) return `No matched regex: ${action.selector}`;
+          el = sourceDom.querySelector(action.selector);
+          if (!el) return `No matched regex: ${action.selector}`;
 
-            let html = el.outerHTML;
-            let regex = new RegExp(action.regex, 'ig');
-            if (html.match(regex, 'ig')) {
-              html = html.replace(regex, action.replace);
-              el.innerHTML = html;
-              return 'Replaced';
-            } else {
-              var temp = document.createElement('template');
-              temp.innerHTML = action.replace;
-              temp.content.childNodes.forEach((node) => {
-                el.appendChild(node);
-              });
-              return `Inserted in ${action.selector}`;
-            }
-          }, action);
+          html = el.outerHTML;
+          regex = new RegExp(action.regex, 'ig');
+          if (html.match(regex, 'ig')) {
+            html = html.replace(regex, action.replace);
+            el.innerHTML = html;
+            message = 'Replaced';
+          } else {
+            newEl = sourceDom.createElement('template');
+            newEl.innerHTML = action.replace;
+            newEl.content.childNodes.forEach((node) => {
+              el.appendChild(node);
+            });
+            message = `Inserted in ${action.selector}`;
+          }
           break;
 
         case 'insertBottom':
-          message = await page.evaluate(async (action) => {
-            let el = document.querySelector(action.selector);
-            if (!el) return `No matched regex: ${action.selector}`;
+          el = sourceDom.querySelector(action.selector);
+          if (!el) return `No matched regex: ${action.selector}`;
 
-            let html = el.outerHTML;
-            var temp = document.createElement('template');
-            temp.innerHTML = action.replace;
-            temp.content.childNodes.forEach((node) => {
-              el.appendChild(node);
-            });
-            return `Inserted in ${action.selector}`;
-          }, action);
+          html = el.outerHTML;
+          newEl = sourceDom.createElement('template');
+          newEl.innerHTML = action.value || '';
+          newEl.content.childNodes.forEach((node) => {
+            el.appendChild(node);
+          });
+          message = `Inserted in ${action.selector}`;
           break;
 
         case 'appendAfter':
-          message = await page.evaluate(async (action) => {
-            let el = document.querySelector(action.selector);
-            if (!el) return `No matched regex: ${action.selector}`;
+          el = sourceDom.querySelector(action.selector);
+          if (!el) return `No matched regex: ${action.selector}`;
 
-            var temp = document.createElement('template');
-            temp.innerHTML = action.value;
-            temp.content.childNodes.forEach((node) => {
-              el.parentNode.insertBefore(node, el.nextSibling);
-            });
-            return;
-          }, action);
+          newEl = sourceDom.createElement('template');
+          newEl.innerHTML = action.value;
+          newEl.content.childNodes.forEach((node) => {
+            el.parentNode.insertBefore(node, el.nextSibling);
+          });
+          message = 'dom appended';
+          break;
+
+        case 'appendStyle':
+          el = sourceDom.querySelector(action.selector);
+          if (!el) return `No matched regex: ${action.selector}`;
+
+          newEl = sourceDom.createElement('style');
+          newEl.appendChild(sourceDom.createTextNode(allStyles));
+          action.attributes.forEach((attr) => {
+            let key, value;
+            [key, value] = attr.split('=');
+            newEl.setAttribute(key, value || '');
+          });
+          el.appendChild(newEl);
+          message = 'styles appended';
           break;
 
         default:
           break;
       }
-      console.log(`---> ${action.log}: ${message}`);
+      console.log(`    ${action.log || action.actionType}: ${message}`.reset);
 
     });
 
-    if (step.beautify) {
-      let html = await page.content();
-      await page.setContent(beautify(html, {
-        indent_size: 2,
-        preserve_newlines: false,
-      }));
-    }
+    let html = sourceDom.documentElement.outerHTML
 
-    await outputToFile(`output-step-${i+1}.html`, await page.content());
+    html = beautify(html, {
+      indent_size: 2,
+      preserve_newlines: false,
+      content_unformatted: ['script', 'style'],
+    });
+    sourceDom.documentElement.innerHTML = html
+
+    await outputToFile(`output-step-${i+1}.html`, html);
+    await page.setContent(html);
     await page.screenshot({path: `output/${escapedUrl}/output-step-${i+1}.png`});
   }
 
   await browser.close();
+  console.log('Complete.'.green);
 }
 
 run();
