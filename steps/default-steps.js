3d582b0896eb3b1d4fa6a7d8aba530ee81c7bced
Jonathan Chen <jonchen@google.com>
2019-06-24 17:41:28 -0400

    Updated default stesps for replacing relative URLs step

--- steps/default-steps.js
+++ steps/default-steps.js
@@ -2,11 +2,23 @@ module.exports = [
   {
     name: 'Make relative URLs absolute',
     actions: [{
+      log: 'Adding https for URLs starts with \/\/',
+      actionType: 'replace',
+      selector: 'head, body',
+      regex: '(href|src)=["\']\/\/([^\)^"^\']*)["\']',
+      replace: '$1="$2"',
+    }, {
       log: 'Update relative URLs',
       actionType: 'replace',
-      selector: 'head > :not(base), body',
+      selector: 'head, body',
       regex: '(href|src)=["\'](\\.*\\/[^\)^"^\']*)["\']',
       replace: '$1="$HOST/$2"',
+    }, {
+      log: 'Update relative URLs in CSS url()',
+      actionType: 'replace',
+      selector: 'style',
+      regex: 'url\\(["\']?(\\.*[^\)^"^\']*)["\']?\\)',
+      replace: 'url($HOST/$1)',
     }],
   },
   {
